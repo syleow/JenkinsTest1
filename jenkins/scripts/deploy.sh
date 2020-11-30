8b0ad064a02378c9939e99cc9e2f56e2c1513051
syleow <1802911@sit.singaporetech.edu.sg>
2020-11-30 16:41:34 +0800

    INT

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name jenkins-tutorials -v /JenkinsTest1 php:7.2-apache
+docker run -d -p 5000:80 --name jenkins-integration -v /JenkinsTest1 php:7.2-apache
 sleep 1
 set +x
 
