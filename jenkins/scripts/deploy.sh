1af0ce2b84e4d263affb6b421b52e51165cfb52d
syleow <1802911@sit.singaporetech.edu.sg>
2020-11-30 15:42:37 +0800

    INT

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\User\Desktop\JenkinsTest1\ php:7.2-apache
+docker run -d -p 80:80 --name jenkins-tutorials -v /JenkinsTest1 php:7.2-apache
 sleep 1
 set +x
 
