1147e9265806db3c17282bed4edfe8636ba5ff94
syleow <1802911@sit.singaporetech.edu.sg>
2020-11-30 15:20:07 +0800

    INTEGRATION

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\User\Desktop\JenkinsTest1\jenkins\scripts php:7.2-apache
 sleep 1
 set +x
 
